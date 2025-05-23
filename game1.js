cb20e5b2d3b1fe92ced68314631a399e5196aede
CinarBayramic <93824489+CinarBayramic@users.noreply.github.com>
2025-05-23 22:37:04 +0300

    added controls to water physics and the code for understanding.

--- game1.js
+++ game1.js
@@ -1,11 +1,13 @@
 const clamp = (val, min, max) => Math.min(Math.max(val, min), max)
 var b_x, b_y;
 var b_r;
-var b_vely;
+var b_vely, b_velx;
 var radiuscontroller;
 function setup() {
   radiuscontroller = createSlider(4, 100);
   radiuscontroller.position(10,410)
+  radiuscontroller.value(30);
+  b_velx = 0;
   b_x = 200;
   b_y = 0;
   b_r = 30;
@@ -15,6 +17,7 @@ function setup() {
 }
 
 function draw() {
+  handleEvents();
   b_r = radiuscontroller.value();
   background(220);
   line(0,200,400,200);
@@ -23,17 +26,33 @@ function draw() {
   fill(122,122,255)
   rect(0,200,400,200);
   fill(255,255,255)
-  ellipse(b_x, b_y%400, b_r, b_r);
+  ellipse(b_x, b_y%400, b_r*2, b_r*2);
   
   
   text("x="+mouseX+"y="+mouseY,mouseX,mouseY)
-  b_y = clamp(b_y + b_vely,0, 400);
-  
+  b_y = clamp(b_y + b_vely,-5000, 400);
+  b_x = clamp(b_x + b_velx,0+b_r, 400 - b_r);
   //batmazlık = yoğunluk * yerçekimi * derinlik(aslında alan ama olsun :])
-  if(b_y>200) {
+  if(b_y+b_r>200) {
     b_vely-= 1*0.01*(b_y-200);
   }
+  b_velx *= 0.99;
   b_vely+=0.1;
   b_vely=clamp(b_vely*0.99,-300,300)
   //b_vely += 9.3;
+}
+function handleEvents() {
+  //handle arrows
+  if (keyIsDown(LEFT_ARROW)) {
+    b_velx -= 0.1;
+  }
+  if (keyIsDown(RIGHT_ARROW)) {
+    b_velx += 0.1;
+  }
+  if (keyIsDown(UP_ARROW)) {
+    b_vely -= 0.06;
+  }
+  if (keyIsDown(DOWN_ARROW)) {
+    b_vely += 0.06;
+  }
 }
